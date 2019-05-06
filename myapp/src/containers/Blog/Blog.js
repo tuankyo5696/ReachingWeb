7bedb7e1bd34a31fb16e9a9bbb030c635fd3d126
Tran Tuan <trananhtuan526201@gmail.com>
2019-05-06 13:10:37 +0700

    first commit

--- myapp/src/containers/Blog/Blog.js
+++ myapp/src/containers/Blog/Blog.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+   state = {
+       posts: []
+   }
+   
+    componentDidMount(){
+         axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.setState({
+                    posts : response.data
+                });  
+              
+            });
+            
+     
+    }
+    
     render () {
+        const {posts} = this.state;
+        const elmposts = posts.map(post =>{
+            return <Post title = {post.title} key={post.id}/>
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {elmposts}
                 </section>
                 <section>
                     <FullPost />
